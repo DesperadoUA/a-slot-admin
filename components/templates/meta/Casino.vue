66c01c4d329a296a7fcf31713e5c14a8dded94b3
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-02-05 11:27:15 +0200

    add all post-type in admin

---
+++ components/templates/meta/Casino.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col class="mt-0">
+                <MM_Multiple_Input
+                    :value = 'data.ref'
+                    :action = 'action' 
+                    :title = '"Referal link"' 
+                    :action_key = '"ref"'
+                    />
+                <MM_Multiple_Input_Text 
+                    :value = 'data.faq'
+                    :action = 'action' 
+                    :title = '"Faq"' 
+                    :action_key = '"faq"'
+                    />
+                <MM_Rating 
+                    :value = 'data.rating'
+                    :action = 'action' 
+                    :title = '"Rating"' 
+                    :action_key = '"rating"'
+                />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import MM_Multiple_Input_Text from '~/components/lib/MM_Multiple_Input_Text'
+    import MM_Input from '~/components/lib/MM_Input'
+    import MM_Rating from '~/components/lib/MM_Rating'
+    import MM_Multiple_Input from '~/components/lib/MM_Multiple_Input'
+    import MM_Checkbox from '~/components/lib/MM_Checkbox'
+    import MM_Multiple_Two_Input from '~/components/lib/MM_Multiple_Two_Input'
+    import MM_Review from '~/components/lib/MM_Review'
+    export default {
+        name: "casinoMeta",
+        props: ['data', 'action'],
+        components: {MM_Multiple_Input_Text, MM_Input, MM_Rating, MM_Multiple_Input, 
+        MM_Checkbox, MM_Multiple_Two_Input, MM_Review},
+        data() {
+            return {}
+        }
+    }
+</script> 
---
+++ components/templates/meta/Vendor.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col class="mt-0">
+                <MM_Rating 
+                    :value = 'data.rating'
+                    :action = 'action' 
+                    :title = '"Rating"' 
+                    :action_key = '"rating"'
+                />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import MM_Input from '~/components/lib/MM_Input'
+    import MM_Rating from '~/components/lib/MM_Rating'
+    export default {
+        name: "vendorMeta",
+        props: ['data', 'action'],
+        components: {MM_Input, MM_Rating},
+        data() {
+            return {}
+        },
+    }
+</script>
