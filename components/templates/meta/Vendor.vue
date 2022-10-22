0b2a268ee84eccbf501cc0b41ab63517dc1cdd1b
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-10-22 09:37:06 +0300

    Add characters for vendors

--- components/templates/meta/Vendor.vue
+++ components/templates/meta/Vendor.vue
@@ -8,6 +8,19 @@
                     :title = '"Rating"' 
                     :action_key = '"rating"'
                 />
+                <MM_Multiple_Options_Three_Input  
+                    :value = 'data.licenses'
+                    :action = 'action'
+                    :items = 'licenses' 
+                    title = "Licenses" 
+                    action_key = "licenses"
+                />
+                <MM_Multiple_Two_Input  
+                    :value = 'data.characteristics'
+                    :action = 'action' 
+                    title = "Characteristics" 
+                    action_key = "characteristics"
+                />
             </v-col>
         </v-row>
     </v-container>
@@ -16,12 +29,17 @@
 <script>
     import MM_Input from '~/components/lib/MM_Input'
     import MM_Rating from '~/components/lib/MM_Rating'
+    import MM_Multiple_Options_Three_Input from '~/components/lib/MM_Multiple_Options_Three_Input'
+    import MM_Multiple_Two_Input from '~/components/lib/MM_Multiple_Two_Input'
+    import config from '~/config'
     export default {
         name: "vendorMeta",
         props: ['data', 'action'],
-        components: {MM_Input, MM_Rating},
+        components: {MM_Input, MM_Rating, MM_Multiple_Options_Three_Input, MM_Multiple_Two_Input},
         data() {
-            return {}
+            return {
+                licenses: config.VENDOR_LICENSES,
+            }
         },
     }
 </script>
--- config.js
+++ config.js
@@ -3,5 +3,6 @@ export default {
     LICENSES: ['ukraine', 'curacao', 'UKGC', 'alderney', 'belgium', 'gibraltar', 'denmark', 'canavec', 'costa_rica', 'malta', 'isle_of_man'],
     PAYMENTS: ['visa', 'qiwi', 'mastercard', 'webMoney', 'maestro', 'yandex', 'bitcoin', 'pm', 'ethereum', 
                'pay', 'privat', 'mono', 'kuna', 'global_money', 'binance', 'binance_pay', 'tether_trc', 'tether_erc', 'adv_cash', 'google_pay'],
-    TYPE_CONTENT: ['default', 'warning', 'attention']
+    TYPE_CONTENT: ['default', 'warning', 'attention'],
+    VENDOR_LICENSES: ['ukraine', 'curacao', 'UKGC', 'alderney', 'belgium', 'gibraltar', 'denmark', 'canavec', 'costa_rica', 'malta', 'isle_of_man']
 }
